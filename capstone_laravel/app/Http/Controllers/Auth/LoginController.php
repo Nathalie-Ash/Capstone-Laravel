549f615ac8a7491588a6b1e7d23287615080ec0b
janabardan <janaalbardan04@gmail.com>
2024-04-29 22:09:13 +0300

    idk

--- capstone_laravel/app/Http/Controllers/Auth/LoginController.php
+++ capstone_laravel/app/Http/Controllers/Auth/LoginController.php
@@ -23,23 +23,12 @@ class LoginController extends Controller
 
     use AuthenticatesUsers;
 
-<<<<<<< HEAD
-    protected function redirectTo()
-    {
-        // Check if the authenticated user is an admin
-
-        
-      
-        return '/dashboard';
-    }
-=======
     /**
      * Where to redirect users after login.
      *
      * @var string
      */
     protected $redirectTo = '/dashboard';
->>>>>>> main
 
     /**
      * Create a new controller instance.
@@ -50,17 +39,7 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
-
     public function showLoginForm()
-<<<<<<< HEAD
-    {
-        return view('login'); // Assuming your custom login view is located at resources/views/auth/login.blade.php
-    }
-
-    // Override the username method to use 'username' field instead of 'email'
-    public function username()
-=======
->>>>>>> main
     {
         return view('auth.login'); // Assuming your custom login view is located at resources/views/auth/login.blade.php
     }
@@ -96,60 +75,10 @@ class LoginController extends Controller
         }
         protected function sendFailedLoginResponse(Request $request)
     {
-<<<<<<< HEAD
-        return $request->only($this->username(), 'password');
-    }
-
-    // Override the login method to customize the authentication logic// Override the login method to customize the authentication logic
-public function login(Request $request)
-{
-    // Validate the form data
-    $request->validate([
-        $this->username() => 'required|string',
-        'password' => 'required|string',
-    ]);
-
-    // Attempt to authenticate the user from the database
-    if (Auth::attempt($this->credentials($request))) {
-        // Authentication passed
-        // if (Auth::user()->is_admin) {
-        //     return redirect('/admin');
-        // } else {
-            return $this->sendLoginResponse($request);
-        // }
-    } else {
-        // Authentication failed
-        return $this->sendFailedLoginResponse($request);
-    }
-}
-
-protected function sendLoginResponse(Request $request)
-{
-    $request->session()->regenerate();
-    return $this->authenticated($request, Auth::user())
-        ?: redirect()->intended($this->redirectPath());
-}
-
-
-    protected function sendFailedLoginResponse(Request $request)
-    {
-        logger($request);
-=======
->>>>>>> main
         throw ValidationException::withMessages([
             $this->username() => [trans('auth.failed')],
         ]);
     }
-<<<<<<< HEAD
-    public function logout(Request $request)
-    {
-        Auth::logout(); // Log the user out
-        $request->session()->invalidate(); // Invalidate the session
-        $request->session()->regenerateToken(); // Regenerate the CSRF token
-        return redirect('/'); // Redirect to the home page or any other page after logout
-    }
-}
-=======
         public function logout(Request $request)
         {
             Auth::logout(); // Log the user out
@@ -159,4 +88,3 @@ protected function sendLoginResponse(Request $request)
         }
     }
     
->>>>>>> main
